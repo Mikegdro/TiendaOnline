d9e22a962064be7ecefa5d29d3213c03c90165b4
Mikegdro <miguelgdro@gmail.com>
2023-02-12 12:42:59 +0100

    Ordenación, falta el rango de edad

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -11,25 +11,81 @@ class AnimalController extends Controller {
     const ORDER_BY = 'animal.name';
     const ORDER_TYPE = 'asc';
 
-    public function index() {
+    public function index(Request $request) {
+
+        //Query
+        $q = $request->input('q', '');
+
+        $orderby = $this->getOrder($this->orderByList(), $request->input('orderby'), self::ORDER_BY);
+
+        $ordertype = $this->getOrder($this->orderTypeList(), $request->input('ordertype'), self::ORDER_TYPE);
+
+        //Página actual
+        $page = $request->input('page', 1);
+
         $animals = DB::table('animal')
-            ->orderBy(SELF::ORDER_BY)
-            ->limit(SELF::ITEMS_PER_PAGE)
+            ->orderBy($orderby, $ordertype)
+            ->limit(self::ITEMS_PER_PAGE)
             ->get();
-        return view('welcome', ['animals' => $animals]);
+
+        return view('welcome', [
+            'animals' => $animals,
+            'order' => $this->getOrderUrls($orderby, $ordertype, $q, 'animal.index'),
+            'q' => $q,
+            'url' => url('/')
+        ]);
     }
 
+    private function getOrderUrls($oBy, $oType, $q, $route) {
+        $urls = [];
 
-    public function create()
-    {
-        //
+        $orderBys = $this->orderByList();
+        $orderTypes = $this->orderTypeList();
+
+        foreach($orderBys as $indexBy => $by) {
+            foreach($orderTypes as $indexType => $type) {
+                if($oBy == $indexBy && $oType == $indexType) {
+                    $urls[$indexBy][$indexType] = url()->full() . '#';
+                } else {
+                    $urls[$indexBy][$indexType] = route($route, [
+                        'orderby'   => $by,
+                        'ordertype' => $type,
+                        'q'         => $q]);
+                }
+            }
+        }
+
+        return $urls;
     }
 
+    /**
+     *
+    */
+    private function getOrder($orderArray, $order, $default) {
+        $value = array_search($order, $orderArray);
 
-    public function store(Request $request)
-    {
-        //
+        if( !$value ) {
+            $value = $default;
+        }
+
+        return $value;
     }
+    private function orderByList() {
+        return [
+            'animal.name' => 'b1',
+            'animal.description' => 'b2',
+            'animal.age' => 'b3',
+            'animal.race' => 'b4'
+        ];
+    }
+
+    private function orderTypeList() {
+        return [
+            'asc'  => 't1',
+            'desc' => 't2',
+        ];
+    }
+
 
 
     public function show(Animal $animal)
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\AnimalController::class, 'index']);
+Route::get('/', [App\Http\Controllers\AnimalController::class, 'index'])->name('animal.index');
 
 
